ddf8b64c7bbc0f2110d04faa3c8bd187cc3431bf
donatborn <103142977+donatborn@users.noreply.github.com>
2025-06-09 16:39:06 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-apt-get update
-apt-get install -y unzip wget
-
-wget -O chrome.zip https://storage.googleapis.com/chrome-for-testing-public/137.0.7151.68/linux64/chrome-linux64.zip
-wget -O chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/137.0.7151.68/linux64/chromedriver-linux64.zip
-
-unzip chrome.zip -d chrome
-unzip chromedriver.zip -d chromedriver_folder
-
-chmod +x ./chromedriver_folder/chromedriver-linux64/chromedriver
-chmod +x ./chrome/chrome-linux64/chrome
-
-mv ./chromedriver_folder/chromedriver-linux64/chromedriver ./chromedriver
-
+export PATH=$PATH:$(pwd)/chrome/chrome-linux64:$(pwd)
 python app.py
