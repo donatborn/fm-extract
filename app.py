de75ee9aa5663c3ebe4b11d01afefeac7f1dc365
donatborn <103142977+donatborn@users.noreply.github.com>
2025-06-09 15:52:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def extract_from_link(link):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
-    # Путь к chromedriver внутри проекта
     service = Service(executable_path="./chromedriver")
 
     driver = webdriver.Chrome(service=service, options=chrome_options)
